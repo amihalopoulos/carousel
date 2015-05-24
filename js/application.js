3b2877e0142219d949c448a007c30eea374a4709
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-24 16:23:29 -0400

    add 5 second interval

--- js/application.js
+++ js/application.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   var images = $('#images').children('li')
 
+  setInterval(function(){ slide(images, "next") }, 5000);
+
   $('#nxt').click(function() {
     slide(images, "next");
   });
