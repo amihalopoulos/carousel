90dcc5203b69a6077470befdaed1200168d43e67
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-24 15:51:16 -0400

    add fadein fade out

--- js/carousel.js
+++ js/carousel.js
@@ -2,25 +2,31 @@
     if (dir === "next"){
       for (var i = 0; i < imageArray.length; i++) {
         if ($(imageArray[i]).attr('class') !== "hidden"){
-          var selected = i + 1
-          if (selected > imageArray.length -1) {
-            selected = 0
+          if (i == imageArray.length -1) {
+            var $selected = $(imageArray[0])
+          } else {
+          var $selected = $(imageArray[i]).next()
           }
-          // $(imageArray[i]).toggle('slide')
+          $(imageArray[i]).fadeOut(800)
+          $selected.fadeIn(800)
           $(imageArray[i]).toggleClass('selected hidden')
-          $(imageArray[selected]).toggleClass('hidden selected')
+          $selected.toggleClass('hidden selected')
+
           break;
         }
       };
     } else if (dir === "previous") {
       for (var i = 0; i < imageArray.length; i++) {
         if ($(imageArray[i]).attr('class') !== "hidden"){
-          var selected = i -1
-          if (selected === -1) {
-            selected = imageArray.length -1
+          if (i == 0) {
+            var $selected = $(imageArray[imageArray.length-1])
+          } else {
+            var $selected = $(imageArray[i]).prev()
           }
+          $(imageArray[i]).fadeOut(800)
+          $selected.fadeIn(800)
           $(imageArray[i]).toggleClass('selected hidden')
-          $(imageArray[selected]).toggleClass('hidden selected')
+          $selected.toggleClass('hidden selected')
           break;
         }
       };
