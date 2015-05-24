a027a0f9d513defdbf3214224dc8dd03dafaa0bc
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-24 16:00:12 -0400

    remove old button events

--- js/application.js
+++ js/application.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
   var images = $('#images').children('li')
-  $('#next').click(function() {
+
+  $('#nxt').click(function() {
     slide(images, "next");
   });
 
   $('#prev').click(function() {
-    slide(images, "previous");
-  });
-
-  $('#previous').click(function() {
     slide(images, "previous");
   });
 
