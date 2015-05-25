0c95325faab0c53f99e1720cf3e8571a384184e9
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-25 10:08:36 -0400

    add opacity of 1 to first image on document ready

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var images = $('#images').children('li')
-
+  var dots = $('#dots').children('li')
+  dots.eq(0).css('opacity', '1')
   setInterval(function(){ slide(images, "next") }, 5000);
 
   $('#nxt').click(function() {
