ea241a7b3c3a46bb19f8ed7938b0e76e0e023281
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-26 09:17:27 -0400

    refactor

--- js/carousel.js
+++ js/carousel.js
@@ -1,37 +1,46 @@
   var slide = function(imageArray, dir) {
     if (dir === "next"){
-      for (var i = 0; i < imageArray.length; i++) {
-        if ($(imageArray[i]).attr('class') !== "hidden"){
-          if (i == imageArray.length -1) {
-            var $selected = $(imageArray[0])
-          } else {
-          var $selected = $(imageArray[i]).next()
-          }
-          $(imageArray[i]).fadeOut(800)
-          $selected.fadeIn(800)
-          $(imageArray[i]).toggleClass('selected hidden')
-          $selected.toggleClass('hidden selected')
-          specifyDot(i, $( "li" ).index( $selected ))
-          break;
-        }
-      };
+      slideRight(imageArray)
     } else if (dir === "previous") {
-      for (var i = 0; i < imageArray.length; i++) {
-        if ($(imageArray[i]).attr('class') !== "hidden"){
-          if (i == 0) {
-            var $selected = $(imageArray[imageArray.length-1])
-          } else {
-            var $selected = $(imageArray[i]).prev()
-          }
-          $(imageArray[i]).fadeOut(800)
-          $selected.fadeIn(800)
-          $(imageArray[i]).toggleClass('selected hidden')
-          $selected.toggleClass('hidden selected')
-          specifyDot(i, $( "li" ).index( $selected ))
-          break;
-        }
-      };
+      slideLeft(imageArray)
     }
+  }
+
+  var slideRight = function(imageArray){
+    for (var i = 0; i < imageArray.length; i++) {
+      if ($(imageArray[i]).attr('class') !== "hidden"){
+        if (i == imageArray.length -1) {
+          var $selected = $(imageArray[0])
+        } else {
+        var $selected = $(imageArray[i]).next()
+        }
+        toggleImages($(imageArray[i]), $selected)
+        specifyDot(i, $( "li" ).index( $selected ))
+        break;
+      }
+    }
+  };
+
+  var slideLeft = function(imageArray) {
+    for (var i = 0; i < imageArray.length; i++) {
+      if ($(imageArray[i]).attr('class') !== "hidden"){
+        if (i == 0) {
+          var $selected = $(imageArray[imageArray.length-1])
+        } else {
+          var $selected = $(imageArray[i]).prev()
+        }
+        toggleImages($(imageArray[i]), $selected)
+        specifyDot(i, $( "li" ).index( $selected ))
+        break;
+      }
+    }
+  };
+
+  var toggleImages = function(oldImage, newImage) {
+    oldImage.fadeOut(800)
+    newImage.fadeIn(800)
+    $(oldImage).toggleClass('selected hidden')
+    newImage.toggleClass('hidden selected')
   }
 
   var specifyDot = function(prev, next) {
@@ -39,3 +48,10 @@
     $("#dots li").eq(prev).css('opacity', '.3')
     $("#dots li").eq(next).css('opacity', '1')
   }
+
+
+// To show a small preview picture in the prev & next divs :
+
+// 1. use prev() and next() to grab those.
+// 2. Try to duplicate them?
+// 3. stick premade classes on them
