074f9ecc071e7bee696b2508f17957062549df39
Alexander Mihalopoulos <amihalop@gmail.com>
2015-09-12 23:27:42 -0400

    refactor

--- js/application.js
+++ js/application.js
@@ -1,16 +1,54 @@
-$(document).ready(function() {
-  var images = $('#images').children('li')
-  var dots = $('#dots').children('li')
-  dots.eq(0).css('opacity', '1')
-  setInterval(function(){ slide(images, "next") }, 5000);
+$(document).ready(function(){
 
-  $('#nxt').click(function() {
-    slide(images, "next");
+
+  //initializations
+  var images = $("#images").children("li");
+  var dots = $("#dots").children("li");
+  var currentNav, currentPic;
+
+
+  //initialize nav
+  $("#dots").find('li').first().addClass('active');
+
+  function goTo(i){
+    $(dots).removeClass('active');
+    $("#dots li").eq(i).addClass('active');
+
+    images.fadeOut(400)
+        .eq(i).fadeIn(400);
+  }
+
+
+
+  //Click on new dots button; make active
+  $("#dots li").on('click',function(){
+    var index = $(this).index();
+    goTo(index);
   });
 
-  $('#prev').click(function() {
-    slide(images, "previous");
+  $("#nxt").on('click',function(){
+    //get current Nav index
+    currentNav = parseInt($('.active').index());
+      if (currentNav <images.length-1){
+        goTo(currentNav+1);
+      } else {
+        goTo(0);
+      }
   });
 
-})
+  $("#prev").on('click',function(){
+    //get current Nav index
+    currentNav = parseInt($('.active').index());
+      if (currentNav >0){
+        goTo(currentNav-1);
+      } else {
+        goTo(images.length-1);
+      }
+  });
+  goTo(0);
 
+  //loop to cycle through
+  setInterval(function(){
+      $("#nxt").trigger('click');
+    },2500);
+});
